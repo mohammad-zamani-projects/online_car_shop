bc0f71db1b6615b46672d44ee20328549c16a045
Amirmohamad <m.zamani@hpds.ir>
2025-08-03 10:58:41 +0330

    add validators to the forms

--- shipping/forms.py
+++ shipping/forms.py
@@ -1,15 +1,23 @@
 from django import forms
-
 from shipping.models import ShippingAddress
+from lib.validators import min_length_validator
+from django.core.exceptions import ValidationError
 
 
 class ShippingAddressForm(forms.ModelForm):
+    # zipcode = forms.CharField(min_length=16, max_length=16)  # kind of validator
+    zipcode = forms.CharField(validators=[min_length_validator])
+
     class Meta:
         model = ShippingAddress
         fields = ['city', 'zipcode', 'address', 'number']  # can use this instead ==>  exclude = ['user']
 
-
-
+    # # validate after jobs!
+    # def clean_zipcode(self):
+    #     zipcode = self.cleaned_data['zipcode']
+    #     if len(zipcode) != 16:
+    #         raise ValidationError("Length of value is not 16")
+    #     return zipcode  # >>This line is SO IMPORTANT<<
 
 
 
