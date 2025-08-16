83e874dd43a4148b3605b2bb470e2a281beae7bc
Amirmohamad <m.zamani@hpds.ir>
2025-08-16 16:51:02 +0330

    Multi-language

--- online_car_shop/settings.py
+++ online_car_shop/settings.py
@@ -102,9 +102,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
+# LANGUAGE_CODE = 'de'  # germany
 
-TIME_ZONE = 'UTC'
+LANGUAGES = (
+    ('en-us', 'English'),
+    ('fa-ir', 'Persian'),
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),  # Translation directory.
+)
+
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
--- package/models.py
+++ package/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext
 
 
 class Package(models.Model):
-    title = models.CharField(max_length=48)
+    title = models.CharField(max_length=48, verbose_name=gettext('title'))
     price = models.PositiveSmallIntegerField()
     description = models.TextField(blank=True)
     days = models.PositiveSmallIntegerField()
@@ -10,6 +11,10 @@ class Package(models.Model):
 
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = gettext('Package')
+        verbose_name_plural = gettext('Packages')
 
     def __str__(self):
         return self.title
